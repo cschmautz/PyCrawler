1f9ef7ba9d310286fc91d30f5e9f2685b6ee7672
Christopher Schmautz <cyanrave@gmail.com>
2017-02-28 19:27:03 -0600

    chore: test new ssh key

---
+++ config.py
@@ -0,0 +1,8 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'c75cb1fa06157cf05b194ac475b559f79ab54f2013f03ace73296ddcc324ce25'
+
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
